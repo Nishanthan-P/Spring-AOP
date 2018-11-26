b5ffa0f6b278fd360137b2d94c155d63a4a34cf8
Nishanthan-P <nishanthan.erode@gmail.com>
2018-11-26 18:11:13 +0530

    First Commit

---
+++ Spring-aop-read-after-finally/src/main/java/com/example/aopdemo/config/AfterThrowingMainApp.java
@@ -0,0 +1,34 @@
+package com.example.aopdemo.config;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.aopdemo.dao.AccountDao;
+import com.example.aopdemo.dao.MembershipDao;
+
+public class AfterThrowingMainApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoAopConfig.class);
+		
+		AccountDao accountDao = context.getBean("accountDao", AccountDao.class);
+		
+		List<Account> accounts = null;
+		Boolean tripWire= true;
+		try{
+			accounts = accountDao.findAccounts(tripWire);
+		}
+		catch(Exception e){
+			System.out.println("\n Main Program.... caught Exception : "+e);
+		}
+		
+		System.out.println("\n\n After Throwing : ");
+		System.out.println("--------");
+		System.out.println("\n"+accounts);
+		
+		context.close();
+	}
+
+}
